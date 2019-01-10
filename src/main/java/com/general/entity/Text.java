a720037c79e0b68ace76ced07c72c28ebf92092e
BarbaraWidjono <barbarawidjono@gmail.com>
2019-01-10 11:52:47 -0800

    Create entity class Text

---
+++ src/main/java/com/general/entity/Text.java
@@ -0,0 +1,51 @@
+package com.general.entity;
+
+public class Text{
+	private String name;
+	private String address;
+	private String resnumber;
+	private String info;
+	private String number;
+	
+	//setters
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	public void setResnumber(String resnumber) {
+		this.resnumber = resnumber;
+	}
+	
+	public void setInfo(String info) {
+		this.info = info;
+	}
+	
+	public void setNumber(String number) {
+		this.number = number;
+	}
+	
+	//getters
+	public String getName() {
+		return name;
+	}
+	
+	public String getAddress() {
+		return address;
+	}
+	
+	public String getResnumber() {
+		return resnumber;
+	}
+	
+	public String getInfo() {
+		return info;
+	}
+	
+	public String getNumber() {
+		return number;
+	}
+}
