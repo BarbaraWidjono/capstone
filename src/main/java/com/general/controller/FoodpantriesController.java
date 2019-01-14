782c543f030663d824fed117bf3882a581461fa5
BarbaraWidjono <barbarawidjono@gmail.com>
2019-01-14 13:38:20 -0800

    Delete record and refresh page

--- src/main/java/com/general/controller/FoodpantriesController.java
+++ src/main/java/com/general/controller/FoodpantriesController.java
@@ -10,19 +10,26 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Optional;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
 
 import com.fasterxml.jackson.databind.JsonNode;
@@ -141,9 +148,14 @@ public class FoodpantriesController{
 	    }	
 	}
 	
-	@PostMapping("/deletefoodpantry")
-	@ResponseStatus(value = HttpStatus.OK)
-	public void deleteFoodpantry(@ModelAttribute Record record) {
-		foodRepository.deleteById(record.getId());
+	@GetMapping("/deletefoodpantry/{id}")
+	public String deleteFoodpantry(@PathVariable("id") Integer id, Model model, Food food) {
+		foodRepository.deleteById(id);
+		
+		List<Food> foods = (List<Food>) foodRepository.findAll();
+		model.addAttribute("foodpantries", foods);
+		
+		return "dashboard";
 	}
+	
 }
