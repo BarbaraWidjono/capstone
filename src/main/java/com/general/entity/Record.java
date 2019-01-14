619ce6e5ac2054e6412be5f5c5ca86bb999dd53b
BarbaraWidjono <barbarawidjono@gmail.com>
2019-01-14 10:25:12 -0800

    New class Record

---
+++ src/main/java/com/general/entity/Record.java
@@ -0,0 +1,13 @@
+package com.general.entity;
+
+public class Record{
+	private Integer id;
+	
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+	public Integer getId() {
+		return id;
+	}
+}
